81abff28b84f196e7e8e637972ef24b2a9f4ef84
the-anirban <77846134+the-anirban@users.noreply.github.com>
2024-06-25 16:35:32 +0530

    f

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,22 +1,14 @@
-var staticCacheName = "pwa";
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/images/coffee1.jpg",
+]
 
-self.addEventListener("install", function (e) {
-e.waitUntil(
-	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll([
-        '/index.html'//,
-        //'/images/logo.png'
-      ]);
-	})
-);
-});
-
-self.addEventListener("fetch", function (event) {
-console.log(event.request.url);
-
-event.respondWith(
-	caches.match(event.request).then(function (response) {
-	return response || fetch(event.request);
-	})
-);
-});
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
